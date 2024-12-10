cc8c76e4fb612e24e503c08df2ba71653ec5379c
Yasser Hajlaoui <yasser159@gmail.com>
2024-12-09 16:09:25 -0800

    Initial Commit

---
+++ moodapp_simple/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  moodapp_simple
+//
+//  Created by Yasser Hajlaoui on 12/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ moodapp_simple/moodapp_simpleApp.swift
@@ -0,0 +1,17 @@
+//
+//  moodapp_simpleApp.swift
+//  moodapp_simple
+//
+//  Created by Yasser Hajlaoui on 12/9/24.
+//
+
+import SwiftUI
+
+@main
+struct moodapp_simpleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
